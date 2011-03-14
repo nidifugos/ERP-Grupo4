180fd31f35a052e9852fcfea412d8de7d05127eb
Rafael <rafael.nidi@gmail.com>
2011-03-14 18:59:19 -0300

    adicionando interfaces básicas de serviços

---
+++ Service/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+
+namespace Microsoft.ServiceModel.Samples
+{
+    class Client
+    {
+    }
+}
---
+++ Service/Server.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+
+namespace Microsoft.ServiceModel.Samples
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            // Step 1 of the address configuration procedure: Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8000/ServiceModelSamples/Service");
+
+            // Step 2 of the hosting procedure: Create ServiceHost
+            ServiceHost selfHost = new ServiceHost(typeof(FornecedorServiços), baseAddress);
+
+            try
+            {
+
+
+                // Step 3 of the hosting procedure: Add a service endpoint.
+                selfHost.AddServiceEndpoint(
+                    typeof(IProvedorServiços),
+                    new WSHttpBinding(),
+                    "FornecedorServiços");
+
+
+                // Step 4 of the hosting procedure: Enable metadata exchange.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5 of the hosting procedure: Start (and then stop) the service.
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                // Close the ServiceHostBase to shutdown the service.
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                Console.ReadLine();
+                selfHost.Abort();
+            }
+
+        }
+    }
+
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface IProvedorServiços
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+
+    public class FornecedorServiços : IProvedorServiços
+    {
+        public double Add(double n1, double n2)
+        {
+            double result = n1 + n2;
+            Console.WriteLine("Received Add({0},{1})", n1, n2);
+            // Code added to write output to the console window.
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Subtract(double n1, double n2)
+        {
+            double result = n1 - n2;
+            Console.WriteLine("Received Subtract({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Multiply(double n1, double n2)
+        {
+            double result = n1 * n2;
+            Console.WriteLine("Received Multiply({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+        public double Divide(double n1, double n2)
+        {
+            double result = n1 / n2;
+            Console.WriteLine("Received Divide({0},{1})", n1, n2);
+            Console.WriteLine("Return: {0}", result);
+            return result;
+        }
+
+    }
+
+}
